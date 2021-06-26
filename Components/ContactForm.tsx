ae875dbb62f1330d6f3aa96d342ff3c07f8db43e
Peter Kerins <pete.kerins@gmail.com>
2021-06-26 19:16:47 +0100

    set up form

--- Components/ContactForm.tsx
+++ Components/ContactForm.tsx
@@ -13,21 +13,23 @@ interface Props {}
 export const ContactForm = (props: Props) => {
   return (
     <Box>
-      <FormControl id="name">
-        <FormLabel>Name</FormLabel>
-        <Input />
-      </FormControl>
-      <FormControl id="email">
-        <FormLabel>Email address</FormLabel>
-        <Input type="email" />
-      </FormControl>
-      <FormControl id="message">
-        <FormLabel>Message</FormLabel>
-        <Textarea placeholder="What's on your mind?" />
-      </FormControl>
-      <Button mt={4} bgColor="brand" color="white" type="submit">
-        Submit
-      </Button>
+      <form name="contact" method="POST" data-netlify="true">
+        <FormControl id="name">
+          <FormLabel>Name</FormLabel>
+          <Input />
+        </FormControl>
+        <FormControl id="email">
+          <FormLabel>Email address</FormLabel>
+          <Input type="email" />
+        </FormControl>
+        <FormControl id="message">
+          <FormLabel>Message</FormLabel>
+          <Textarea placeholder="What's on your mind?" />
+        </FormControl>
+        <Button mt={4} bgColor="brand" color="white" type="submit">
+          Submit
+        </Button>
+      </form>
     </Box>
   );
 };
