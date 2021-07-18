35497e1a8b1d34c2f875dd3b171cd4a2ec81dc2c
Peter Kerins <pete.kerins@gmail.com>
2021-07-18 22:32:29 +0100

    use new active links, edit main logo styles

--- Components/Navigation.tsx
+++ Components/Navigation.tsx
@@ -1,34 +1,28 @@
 import React from "react";
-import Link from "next/link";
-import { Heading, Flex, Spacer, Stack } from "@chakra-ui/react";
+import NextLink from "next/link";
+import { Heading, Flex, Spacer, Stack, Link } from "@chakra-ui/react";
 import ActiveLink from "./ActiveLink";
 interface Props {}
 
 const Navigation = (props: Props) => {
   return (
     <Flex
-      maxWidth="7xl"
+      maxWidth="5xl"
       mx="auto"
       my={4}
       alignContent="center"
       alignItems="center"
     >
       <Heading fontSize={{ base: "xl", md: "2xl", lg: "4xl" }}>
-        <Link href="/">
-          <a className="nav-link">Peter Kerins</a>
-        </Link>
+        <NextLink href={"/"} passHref>
+          <Link _hover={{ textDecoration: "none" }}>Peter Kerins</Link>
+        </NextLink>
       </Heading>
       <Spacer />
-      <Stack direction="row">
-        <ActiveLink href="/">
-          <a>Home</a>
-        </ActiveLink>
-        <ActiveLink href="/about">
-          <a>About</a>
-        </ActiveLink>
-        <ActiveLink href="/contact">
-          <a>Contact</a>
-        </ActiveLink>
+      <Stack direction="row" spacing="4">
+        <ActiveLink href="/" label="Home" />
+        <ActiveLink label="About" href="/about" />
+        <ActiveLink label="Contact" href="/contact" />
       </Stack>
     </Flex>
   );
