8609f7de2e99118866df1a2c53137f4b7fa84848
Peter Kerins <pete.kerins@gmail.com>
2021-07-19 20:18:50 +0100

    Contact form up and running

--- Components/ContactForm.tsx
+++ Components/ContactForm.tsx
@@ -7,31 +7,74 @@ import {
   FormLabel,
   Input,
   Textarea,
+  useToast,
 } from "@chakra-ui/react";
+import { useForm } from "react-hook-form";
 interface Props {}
 
 export const ContactForm = (props: Props) => {
+  const toast = useToast();
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+    reset,
+  } = useForm({
+    defaultValues: {
+      name: "",
+      email: "",
+      message: "",
+    },
+  });
+
+  function onSubmit(data: { name: string; email: string; message: string }) {
+    fetch("/api/contact", {
+      method: "post",
+      body: JSON.stringify(data),
+    })
+      .then(function (response) {
+        if (!response.ok) {
+          throw Error(response.statusText);
+        }
+        return response;
+      })
+      .then(function (response) {
+        toast({
+          status: "success",
+          title: "Message Received",
+          position: "bottom",
+        });
+        reset();
+      })
+      .catch(function (error) {
+        toast({
+          status: "error",
+          title: "Oh No!",
+          description: "Something went wrong, let's try that again",
+          position: "bottom",
+        });
+      });
+  }
   return (
     <Box>
-      <form
-        name="contact"
-        method="POST"
-        action="/form-success"
-        data-netlify="true"
-      >
+      <form onSubmit={handleSubmit(onSubmit)}>
         <input type="hidden" name="form-name" value="contact" />
-        <FormControl id="name">
+        <FormControl id="name" isInvalid={!!errors?.name}>
           <FormLabel>Name</FormLabel>
-          <Input name="name" required />
+          <Input {...register("name", { required: "Name is requred" })} />
         </FormControl>
-        <FormControl id="email">
+        <FormControl id="email" isInvalid={!!errors.email}>
           <FormLabel>Email address</FormLabel>
-          <Input type="email" name="email" required />
+          <Input
+            type="email"
+            {...register("email", { required: "Email is required" })}
+            required
+          />
         </FormControl>
-        <FormControl id="message">
+        <FormControl id="message" isInvalid={!!errors.message}>
           <FormLabel>Message</FormLabel>
           <Textarea
-            name="message"
+            {...register("message", { required: true })}
             required
             placeholder="What's on your mind?"
           />
---
+++ pages/api/contact.ts
@@ -0,0 +1,40 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import mail from "@sendgrid/mail";
+
+if (process.env.SENDGRID_API_KEY) {
+  mail.setApiKey(process.env.SENDGRID_API_KEY);
+} else {
+  throw new Error("SENDGRID API KEY NOT FOUND");
+}
+
+const contact = async (req: NextApiRequest, res: NextApiResponse) => {
+  const body = JSON.parse(req.body);
+
+  const message = `
+    Name: ${body.name}\r\n
+    Email: ${body.email}\r\n
+    Message: ${body.message}
+  `;
+  const data = {
+    to: "hello@peterkerins.com",
+    from: "hello@peterkerins.com",
+    subject: `New message from ${body.name}`,
+    text: message,
+    html: message.replace(/\r\n/g, "<br />"),
+  };
+
+  (async () => {
+    try {
+      await mail.send(data);
+    } catch (error) {
+      console.error(error);
+      if (error.response) {
+        console.error(error.response.body);
+      }
+    }
+  })();
+
+  res.status(200).json({ status: "OK" });
+};
+
+export default contact;
