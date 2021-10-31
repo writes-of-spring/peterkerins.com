ed67fa8a16bb344979e8559c3238407f83823698
Peter Kerins <pete.kerins@gmail.com>
2021-10-31 09:45:37 +0000

    remove unused imports in homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,9 @@
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
-import { Box, Text } from "@chakra-ui/react";
-import { Service } from "../Components/Service";
-import React from "react";
-import Banner from "../Components/Banner";
-import { Services } from "Components/Services";
+import { Box } from "@chakra-ui/react";
 import { ContactForm } from "Components/ContactForm";
 import Section from "Components/Section";
+import { Services } from "Components/Services";
+import React from "react";
+import Banner from "../Components/Banner";
 export default function Home() {
   return (
     <Box bg="non" fontSize="green">
